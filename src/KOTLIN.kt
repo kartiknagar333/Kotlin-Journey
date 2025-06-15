9ba4eb6d58693869047b2c38f727baac9a404aa8
Kartiknagar333 <kartiknagar20642064@gmail.com>
2025-06-15 15:15:04 -0500

    variables

---
+++ src/KOTLIN.kt
@@ -0,0 +1,25 @@
+// File: basic.kt
+
+// Define a top-level constant
+const val KOTLIN = "KOTLIN"
+
+// Function that performs the logic
+fun runBasicExample() {
+    // Print the constant
+    println(KOTLIN)  // Output: KOTLIN
+
+    // `var` is mutable
+    var a = 5
+    a = 4  // Reassigned
+    println(a)  // Output: 4
+
+    // `val` can be assigned only once
+    val b: Int
+    b = 6
+    // b = a  // ❌ This would cause an error
+    println(b)  // Output: 6
+
+    // Create a lowercase version of the constant
+    val kotlin = KOTLIN.lowercase()
+    println(kotlin)  // Output: kotlin
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 
+import runBasicExample
 
 fun main() {
-
+    runBasicExample()
 }
